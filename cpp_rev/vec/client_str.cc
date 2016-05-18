7f279cbd0abae1da67c1faec906fa35be82bebcb
Kashyap Maduri <kashyapmaduri@outlook.com>
2016-05-17 20:08:15 -0700

    adding some more TCs. copied gitignore from acpp_progs

--- cpp_rev/vec/client_str.cc
+++ cpp_rev/vec/client_str.cc
@@ -8,6 +8,9 @@ using std::cin;
 int main()
 {
     Str s1("hello");
+    Str delim(20, '#');
+
+    delim = "\n" + delim;
 
     cout << "s1 = (" << s1 << ")" << endl;
     cout << "please enter your name: ";
@@ -15,4 +18,20 @@ int main()
     cin >> name;
 
     cout << "Hello " +  name + "!" << endl;
+
+    Vec<char> vowels;
+    vowels.push_back('a');
+    vowels.push_back('e');
+    vowels.push_back('i');
+    vowels.push_back('o');
+    vowels.push_back('u');
+
+    cout << delim << endl;
+    cout << "building a vowel string!" << endl;
+    Str vowel_str(vowels.begin(), vowels.end());
+
+    cout << "vowel_str --> " << endl;
+    cout << vowel_str << endl;
+    cout << delim << endl;
+
 }
